ba94a92d6a5da080eed08b22bbb64e98f7de9482
Tatenda <tchitak1@stu.vistula.edu.pl>
2025-03-01 17:08:18 +0100

    I am counting to 10 instead of 5.

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello Tatenda!");
 
-        for (int i = 1; i <= 5; i++) {//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        for (int i = 1; i <= 10; i++) {//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
